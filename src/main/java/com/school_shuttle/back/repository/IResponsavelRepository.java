357858a6c3f1f9a4be067258cd9791581a443857
yRaposo <ynyght@gmail.com>
2025-05-05 21:57:14 -0300

    feat: Cria repositório IResponsavelRepository para gerenciamento de Responsavel

--- src/main/java/com/school_shuttle/back/repository/IResponsavelRepository.java
+++ src/main/java/com/school_shuttle/back/repository/IResponsavelRepository.java
@@ -5,14 +5,14 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.school_shuttle.back.model.Responsavel;
-import com.school_shuttle.back.model.Usuario;
+
 import java.util.List;
 
-public interface IResponsavel extends JpaRepository<Responsavel, Long> {
+public interface IResponsavelRepository extends JpaRepository<Responsavel, Long> {
 
     public Optional<Responsavel> findById(Long id);
 
-    public Optional<Responsavel> findByUser_Id(Long userId);
+    public List<Responsavel> findByUser_Id(Long userId);
 
     public Optional<Responsavel> findByNomeCrianca(String nomeCrianca);
 }
