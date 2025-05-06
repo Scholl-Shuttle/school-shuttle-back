b38f8dc849c7a5136b469d67de9129d120eacef3
yRaposo <ynyght@gmail.com>
2025-05-05 21:12:13 -0300

    refactor: Adiciona quebras de linha para melhor legibilidade no repositório IResponsavel

--- src/main/java/com/school_shuttle/back/repository/IResponsavel.java
+++ src/main/java/com/school_shuttle/back/repository/IResponsavel.java
@@ -9,9 +9,13 @@ import com.school_shuttle.back.model.Responsavel;
 public interface IResponsavel extends JpaRepository<Responsavel, Long> {
 
     Optional<Responsavel> findByEmail(String email);
+
     Optional<Responsavel> findById(Long id);
+
     Optional<Responsavel> findByNome(String nome);
+
     Optional<Responsavel> findByTelefone(String telefone);
+
     Optional<Responsavel> findByCpf(String cpf);
 
 }
